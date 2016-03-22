5406be4410846754d37321a5bc9c7d1b7316d7b2
nikitastrelkov <nikita.strelkov@gmail.com>
2016-03-22 23:06:25 +0500

    Add and install packages and configure installed packages
    Install elasticsearch php client and bundle for comfort work with it
    Configure application:
    - configure db options
    - enable serializer
    - set elastica ip addres and port
    - configure FOSRestBundle to work with json on requests to /api and some
    more standart configs

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,10 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new FOS\ElasticaBundle\FOSElasticaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
