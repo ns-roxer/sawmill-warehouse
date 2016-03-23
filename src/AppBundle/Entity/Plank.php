8954d56dd457330c0307f2eda327ef4b5a7293ec
nikitastrelkov <nikita.strelkov@gmail.com>
2016-03-23 10:52:06 +0500

    Fill Plank model with necessary fields and relations

--- src/AppBundle/Entity/Plank.php
+++ src/AppBundle/Entity/Plank.php
@@ -20,7 +20,50 @@ class Plank
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
+    
+    /**
+     * @var Color
+     *
+     * @ORM\ManyToOne(targetEntity="Color")
+     * @ORM\JoinColumn(name="color_id", referencedColumnName="id", nullable=false)
+     */
+    private $color;
 
+    /**
+     * @var Material
+     *
+     * @ORM\ManyToOne(targetEntity="Material")
+     * @ORM\JoinColumn(name="material_id", referencedColumnName="id", nullable=false)
+     */
+    private $material;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="length", type="float", nullable=false)
+     */
+    private $length;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="height", type="float", nullable=false)
+     */
+    private $height;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="width", type="float", nullable=false)
+     */
+    private $width;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="quantity", type="integer", nullable=false)
+     */
+    private $quantity;
 
     /**
      * Get id
@@ -30,6 +73,102 @@ class Plank
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * @return Color
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param Color $color
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    /**
+     * @return Material
+     */
+    public function getMaterial()
+    {
+        return $this->material;
+    }
+
+    /**
+     * @param Material $material
+     */
+    public function setMaterial($material)
+    {
+        $this->material = $material;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLength()
+    {
+        return $this->length;
+    }
+
+    /**
+     * @param float $length
+     */
+    public function setLength($length)
+    {
+        $this->length = $length;
+    }
+
+    /**
+     * @return float
+     */
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    /**
+     * @param float $height
+     */
+    public function setHeight($height)
+    {
+        $this->height = $height;
+    }
+
+    /**
+     * @return float
+     */
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    /**
+     * @param float $width
+     */
+    public function setWidth($width)
+    {
+        $this->width = $width;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param int $quantity
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
     }
 }
 
