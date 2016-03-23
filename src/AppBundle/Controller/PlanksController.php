306f9c54419a1b15ebfc840b915bb8a59b46b5fa
nikitastrelkov <nikita.strelkov@gmail.com>
2016-03-23 12:55:37 +0500

    Fix configs, dependencies and add controllers
    Remove elasticsearch-php, because FOSElasticaBundle do the same and more
    Add access_token param
    Add plank controller methods for get plank, get all planks and delete
    pank

--- src/AppBundle/Controller/PlanksController.php
+++ src/AppBundle/Controller/PlanksController.php
@@ -2,23 +2,81 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Plank;
+use Doctrine\ORM\EntityManager;
+use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
 
 class PlanksController extends FOSRestController
 {
     /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \InvalidArgumentException
+     * @param $id
+     *
+     * @return \AppBundle\Entity\Plank
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      */
     public function getPlankAction($id)
     {
+        /** @var Plank $plank */
+        $plank = $this->getDoctrine()
+            ->getRepository('AppBundle:Plank')
+            ->find($id);
 
-        return ['hello' => 'world'];
+        if ($plank === null) {
+            throw $this->createNotFoundException('No such plank');
+        }
+
+        return $plank;
     }
 
+    /**
+     * @return \AppBundle\Entity\Plank[]|array
+     */
     public function getPlanksAction()
     {
+        /** @var Plank $plank */
+        $planks = $this->getDoctrine()
+            ->getRepository('AppBundle:Plank')
+            ->findAll();
 
-        return ['yes'];
+        return $planks;
+    }
+
+    /**
+     * @View(statusCode=204)
+     *
+     * @param $id
+     *
+     * @return \FOS\RestBundle\View\View
+     *
+     * @throws \InvalidArgumentException
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     * @throws \LogicException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Doctrine\ORM\ORMInvalidArgumentException
+     */
+    public function deletePlankAction(Request $request, $id)
+    {
+        /** @var string | null $token */
+        $token = $request->query->get('token');
+
+        if ( !$token || $token !== $this->getParameter('auth_token')) {
+            throw $this->createAccessDeniedException();
+        }
+
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+        $plank = $em->getRepository('AppBundle:Plank')->find($id);
+
+        if ($plank === null) {
+            throw $this->createNotFoundException('No such plank');
+        }
+
+        $em->remove($plank);
+        $em->flush();
+
+        return $this->view([], 204);
     }
 }
